7768d7d943641df5cab91982f9e6628e7d13693b
penguinsausager <barrybooo2@gmail.com>
2024-10-10 16:52:19 +0500

    nullable types + elvis and Unit

--- app/src/main/java/com/example/myapplication/corporation/Corporation.kt
+++ app/src/main/java/com/example/myapplication/corporation/Corporation.kt
@@ -1,20 +1,11 @@
 package com.example.myapplication.corporation
 
 fun main() {
-    val assistant = WorkersRepository.findAssistant()
-    assistant?.printInfo()
-
     val director = WorkersRepository.findDirector()
-    if (assistant != null) {
-        director?.takeCoffee(assistant)
-    }
-
-    val directorSalary = director?.salary ?: 0
-    val assistantSalary = assistant?.salary ?: 0
-    val a = method()
-
-
+        ?: throwDirectorIsRequired()
+    director?.printInfo()
 }
-fun method(){
 
+fun throwDirectorIsRequired() : Nothing{
+    throw IllegalArgumentException("Director is required for this program")
 }
